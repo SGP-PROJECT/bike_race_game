1645e3dc50dde3c867d8c2932907cd167ce36895
Aum9007 <72548293+Aum9007@users.noreply.github.com>
2020-10-10 10:08:22 +0530

    Add files via upload

---
+++ constants.h
@@ -0,0 +1,17 @@
+     const int WIDTH=800;
+     const int HEIGHT=600;
+     const int BPP=32;
+
+
+     const int START_LAYER_Y = 530;
+
+
+     const int START_BIKE_X=150;
+     const int FINISH_BIKE_X=250;
+
+
+     const int LAYER_Y=493;
+
+     const int SMAX_WALL=5;
+     const int BMAX_WALL=4;
+
